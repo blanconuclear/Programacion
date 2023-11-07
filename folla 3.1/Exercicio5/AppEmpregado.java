0d3cd63f592ff1b98cd5df76ba2da0835b0fbc0e
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2023-11-07 19:57:37 +0100

    exercicio 5 correxido

--- folla 3.1/Exercicio5/AppEmpregado.java
+++ folla 3.1/Exercicio5/AppEmpregado.java
@@ -1,28 +1,26 @@
-package Exercicio5;
+package exercicio5;
 
 public class AppEmpregado {
     public static void main(String[] args) {
-        Empregado empregado1 = new Empregado("Jose", "Gonzalez", "56844567G", 1230);
+
+        Empregado empregado1 = new Empregado();
         Empregado empregado2 = new Empregado();
 
-        System.out
-                .println(empregado1.nome + " " + empregado1.apelido + " con DNI: " + empregado1.DNI
-                        + " .Ten un salario de: "
-                        + empregado1.salario);
-        System.out
-                .println(empregado2.nome + " " + empregado2.apelido + " con DNI: " + empregado2.DNI
-                        + " .Ten un salario de: "
-                        + empregado2.salario);
+        empregado2.setNome("toño");
+        empregado2.setApelido("rodriguez");
+        empregado2.setDni("33443234T");
+        empregado2.setSalario(2000);
 
-        empregado1.setNome("Lolo");
-        empregado1.setApelido("Dominguez");
-        empregado1.setDNI("33243677J");
-        empregado1.setSalario(1450);
+        System.out.println(empregado2.getNome());
+        System.out.println(empregado2.getApelido());
+        System.out.println(empregado2.getDni());
+        System.out.println(empregado2.getSalario());
 
-        System.out
-                .println(empregado1.getNome() + " " + empregado1.getApelido() + " con DNI: " + empregado1.getDNI()
-                        + " .Ten un salario de: "
-                        + empregado1.getSalario());
+        System.out.println("_____________________");
 
+        System.out.println(empregado1.nome);
+        System.out.println(empregado1.apelido);
+        System.out.println(empregado1.dni);
+        System.out.println(empregado1.salario);
     }
 }
--- folla 3.1/Exercicio5/Empregado.java
+++ folla 3.1/Exercicio5/Empregado.java
@@ -1,30 +1,17 @@
-/*Crea unha clase chamada Empregado, que conteña atributos públicos para o nome, os apelidos, o DNI e o salario. 
-Engade un construtor sen argumentos, que poña uns valores "razoables" neses atributos. 
-Crea varios métodos setNome(String nome ), setApelidos(String apelido), setDni(... ), e setSalario(... ), que modifiquen os valores cos argumentos. 
-Crea 2 empregados e comproba que os métodos fan o que deben facer. */
-
-package Exercicio5;
+package exercicio5;
 
 public class Empregado {
 
-    // Atributos
     public String nome;
     public String apelido;
-    public String DNI;
+    public String dni;
     public int salario;
 
     public Empregado() {
-        nome = "Paco";
-        apelido = "Corzo";
-        DNI = "33567578G";
-        salario = 2000;
-    }
-
-    public Empregado(String nome, String apelido, String DNI, int salario) {
-        this.nome = nome;
-        this.apelido = apelido;
-        this.DNI = DNI;
-        this.salario = salario;
+        nome = "Andrés";
+        apelido = "Gonzalez";
+        dni = "99485734Y";
+        salario = 1230;
     }
 
     public void setNome(String nome) {
@@ -35,8 +22,8 @@ public class Empregado {
         this.apelido = apelido;
     }
 
-    public void setDNI(String DNI) {
-        this.DNI = DNI;
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 
     public void setSalario(int salario) {
@@ -51,8 +38,8 @@ public class Empregado {
         return apelido;
     }
 
-    public String getDNI() {
-        return DNI;
+    public String getDni() {
+        return dni;
     }
 
     public int getSalario() {
