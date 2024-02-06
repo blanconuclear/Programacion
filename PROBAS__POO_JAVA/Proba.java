639ceba66b3b6bb4b84a36d7c1df8faf7aa9722e
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2024-02-06 21:47:48 +0100

    correcicones

---
+++ PROBAS__POO_JAVA/Proba.java
@@ -0,0 +1,11 @@
+public class Proba {
+
+    public static void saludo(String nome) {
+        System.out.println("hola " + nome);
+    }
+
+    public static void main(String[] args) {
+        saludo("Pere");
+
+    }
+}
