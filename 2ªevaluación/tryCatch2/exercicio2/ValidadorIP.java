8072d5c3fa44358453421171d9f91c7481c71176
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2024-02-08 21:06:10 +0100

    exercicio finalizado

--- 2ªevaluación/tryCatch2/exercicio2/ValidadorIP.java
+++ 2ªevaluación/tryCatch2/exercicio2/ValidadorIP.java
@@ -1,7 +1,5 @@
 package exercicio2;
 
-import java.util.Scanner;
-
 /**
  * Crea un validador de direcciones IP. Lembra que as direcciones IP son
  * especificadas en decimal como 4 grupos de números separados por ‘.’ Cada
@@ -17,36 +15,43 @@ import java.util.Scanner;
  * 0.1.2.3.
  * 256.1.2.3
  */
+import java.util.Scanner;
+
 public class ValidadorIP {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Escribe a tua dirección IP: ");
         String direccionIP = sc.nextLine();
 
         String[] words = direccionIP.replace(".", " ").split(" ");
+        boolean eValida = true;
 
         for (String string : words) {
-            int proba = Integer.parseInt(string);
-            if (proba > 0 && proba < 250) {
-                System.out.println("A IP é correcta: " + direccionIP);
-            } else {
-                System.out.println("A direccion non é correcta");
+            try {
+                int proba = Integer.parseInt(string);
+                if (words.length != 4) {
+                    eValida = false;
+                } else {
+
+                    if (!(proba >= 0 && proba <= 255)) {
+                        eValida = false;
+                        break;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                eValida = false;
+                break;
             }
         }
+
+        if (eValida) {
+            System.out.println("A dirección: " + direccionIP + " é correcta");
+        } else {
+            System.out.println("A dirección: " + direccionIP + " non é correcta");
+        }
+
+        sc.close();
     }
+
 }
-
-/*
- * for (String word : words) {
- * System.out.println(word);
- * }
- */
-
-/*
- * String convertirString = words.toString();
- * int convertirNumero = Integer.parseInt(convertirString);
- * 
- * System.out.println(convertirString);
- */
