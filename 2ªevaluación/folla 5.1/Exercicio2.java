fd4580733d84733a0059b09cb687b495fdd2568a
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2024-02-20 21:04:05 +0100

    exercicio 2 finalizado

--- 2ªevaluación/folla 5.1/Exercicio2.java
+++ 2ªevaluación/folla 5.1/Exercicio2.java
@@ -1,3 +1,4 @@
+
 /*
  * . Fai un programa que pida o nome dun ficheiro con JOptionPane, e se existe o
  * copie nunha carpeta
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import javax.swing.JOptionPane;
 
@@ -18,14 +20,16 @@ public class Exercicio2 {
 
     public static void main(String[] args) throws IOException {
         String nomeFicheiro = JOptionPane.showInputDialog("Dime o nome do ficheiro");
-        String rutaFinal = "/home/sanclemente.local/a23efrencv/Descargas";
+        String rutaFinal = "/home/sanclemente.local/a23efrencv/Descargas/copia";
 
         Path path = Paths.get(nomeFicheiro);
+        Path destino = Paths.get(rutaFinal, nomeFicheiro);
 
         if (Files.exists(path)) {
             System.out.println("O ficheiro existe");
 
-            Files.copy(path, null);
+            Files.copy(path, destino, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Ficheiro copiado con éxito a: " + destino);
 
         } else {
             System.out.println("O ficheiro NON existe");
