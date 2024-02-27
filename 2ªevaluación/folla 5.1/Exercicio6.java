5028bb99541f26cf54682c2c4112668cc0ddb757
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2024-02-27 20:51:50 +0100

    exercicio6

--- 2ªevaluación/folla 5.1/Exercicio6.java
+++ 2ªevaluación/folla 5.1/Exercicio6.java
@@ -5,12 +5,36 @@
  * “exemplo2.txt”, etc.
  */
 
-/**
- * Exercicio6
- */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class Exercicio6 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String nomeCarpeta = "ficheirosTemporais";
+        String ruta = "/Users/efrencorzonvazquez/Desktop";
+
+        File carpeta = new File(ruta + "/" + nomeCarpeta);
+        boolean creado = carpeta.mkdirs();
+
+        if (creado) {
+            System.out.println("Carpeta creada con éxito");
+        } else {
+            System.out.println("A carpeta xa existe.");
+        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Escribe a frase número " + (i + 1) + ": ");
+            String frase = sc.nextLine();
+
+            String rutaArchivo = ruta + "/" + nomeCarpeta + "/exemplo" + (i + 1) + ".txt";
+            File archivo = new File(rutaArchivo);
+            archivo.createNewFile();
+            Files.write(archivo.toPath(), frase.getBytes());
+        }
+        sc.close();
     }
-
 }
